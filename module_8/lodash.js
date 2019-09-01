d9944e9c1a440b3ccbaf308e7884c9abafd3aac6
Arkhipenka <europeoplebelarus@gmail.com>
2019-09-01 19:32:47 +0300

    inpemented chunk, omit, pick, zip

--- module_8/lodash.js
+++ module_8/lodash.js
@@ -1,103 +1,140 @@
+//доработать
+function сhunck(arr, n){
+    n = n || 1;
+    let arrResult = [];
+    for (let i = 0; i < arr.length; i += n){
+        arResult.push(arr[i]);
+    }
+    return arrResult;
+}
+
 function compact(arr) {
-	let array = [];
+	let arrResult = [];
 	for (let i = 0; i < arr.length; i++){
 		if(arr[i]){
-			array.push(arr[i]);
+			arrResult.push(arr[i]);
 		}
 	}
-	return array;
+	return arrResult;
 }
 
-function drop(arr){
-	arr.shift();
-	return arr;
-}
-
-function dropWhile(arr, n){
-	let array = [];
+function drop(arr, n){
+	n = n || 1;
+	let arrResult = [];
 	for (let i = n; i < arr.length; i++) {
-		array.push(arr[i]);
+		arrResult.push(arr[i]);
 	}
-	return array;
+	
+	return arrResult;
+}
+// доработать
+function dropWhile(arr, predicate){
+	let arrResult = [];
+	for (let i = -1; ++i < arr.length && predicate(arr[i]); i++) {
+		arResult.push(arr[i]);
+	}
+	return arrResult;
 }
 
-function filter(arr, value){
-  
-    let array = [];
+function filter(arr, predicate){
+    let arrResult = [];
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === value ){
-        array.push(arr[i]);
+      let parrametrBoolean = predicate(arr[i]);
+      if(parrametrBoolean){
+      	arrResult.push(arr[i]);
       }
     }
-    return array;
+    return arrResult;
 }
 
 
-function chunk(arr, n){
-    let arrArr = [];
-	let array = [];
-	
-		for (let i = 0; i < arr.length; i++) {
-			if(arrArr.length < n){
-				arrArr.push(arr[i])
-			}
-	}
-	array.push(arrArr);
-	    
-    
-	return array;
-}
-	    
-function find(arr, value){
-	let array = [0];
-	for (var i = 0; i < arr.length; i++) {
-		if(arr[i] === value){
-			array = arr[i];
+
+   
+function find(arr, predicate, n){
+	let answer;
+	n = n || 0;
+	for (let i = n; i < arr.length; i++) {
+		let booleanItem = predicate(arr[i]);
+		if(booleanItem){
+			answer = arr[i];
 		}
-		return array;
+		return answer;
 	}
 }
 
 function take(arr, n){
-	let array = [];
-	for (var i = 0; i < n; i++) {
-		array.push(arr[i]);
+	let size = n || 1;
+	let arrResult = [];
+	for (let i = 0; i < size; i++) {
+		arrResult.push(arr[i]);
 	}
-	return array;
+	return arrResult;
 }
-
-function includes(arr, value){
-	let arrBoolean;
-	for (var i = 0; i < arr.length; i++) {
-		if(arr[i] === value){
-			arrBoolean = true;
-		}
-
-		arrBoolean = false;
-	}
-	return arrBoolean;
-}
-function zip(arr){
-	let array = [];
-	for (var i = 1; i < arr.length; i++) {
-		let itemArr = arr[i];
-		for (var j = 0; j < itemArr.length; j++) {
-			if(i === j){
-				array.push(itemArr[j]);
+//доделать
+function includes(arr, value, n){
+	let answer = false;
+	if (!n) {
+		n = 0;
+	}else if(n < 0){
+		n = arr.length + n - 1;
+	}else if(typeOf(value) === 'string' && typeOf(arr) === 'string'){
+		return arr.includes(value, n);
+	}else if(n >= 0){
+		for (var i = n; i < arr.length; i++) {
+			if(arr[i] === value){
+				answer = true;
+				break;
 			}
 		}
-		return array
-
-	}return array;
+	}else{
+		for (let i = n; i > 0; i--){
+			if(value == arr[i]){
+				answer = true;
+				break;
+			}
+		}
+	}
+	
+	return answer;
 }
 
-function map(arr, operation){
-	let array = [];
+function map(arr, iteratee){
+	let arrResult = [];
 	for (var i = 0; i < arr.length; i++) {
-		let parameter = operation;
-		return parameter;
-		array.push(arr[i])
+    let itemArr = iteratee(arr[i]);
+		arrResult.push(itemArr)
+	}
+	return arrResult;
+}
+//доработать
+function zip(...arr){
+	let arrResult = [];
+	arrResult.length = arr[0].length;
+	let itemArr = [];
+	for (var i = 1; i < arr.length; i++) {
+		for (let array of arr) {
+			
+				itemArr.push(array[i]);
+	
+		}
+		arrResult[i].push(itemArr)
+		
 
 	}
-	return array;
+	return arrResult;
+}
+
+
+
+function merge(arg, arg1){
+
+
+}
+
+function pick(arg1, arg2, ... argn){
+
+}
+
+function omitBy(){
+
 }

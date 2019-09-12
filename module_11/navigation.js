3b3c60a7e300179f785a2367f36debb83ab41922
Arkhipenka <europeoplebelarus@gmail.com>
2019-09-12 09:08:01 +0300

    inplemented nav

--- module_11/navigation.js
+++ module_11/navigation.js
@@ -1,5 +1,17 @@
+window.addEventListener('load', function(event){
+	let nav = document.createElement('nav');
+	let arrNav = ["menu", "news", "setting", "on demand"];
+	nav.id = "nav";
 
-function nav(argument) {
-	// body...
-	document.body.innerHTML('<nav><h1>hi</h1></nav>');
-}
+	for(let i = 0; i < arrNav.length; i++){
+		nav.innerHTML += "<a href='#'>" + arrNav[i] + "</a>";
+		
+	}
+	document.body.insertBefore(nav,document.body.firstChild);
+	//form.innerHTML = "<a href='#'>General</a>";
+		//document.body.insertBefore(form, document.body.firstChild);
+
+		//let div = document.createElement('div');
+				//div.id = 'div';
+		//document.body.appendChild(div);
+})

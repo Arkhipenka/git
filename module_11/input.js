f97ecddf7f9ef7d97cc660d817c2c83761824c1d
Arkhipenka <europeoplebelarus@gmail.com>
2019-09-26 17:07:35 +0300

    implemented notes

--- module_11/input.js
+++ module_11/input.js
@@ -2,45 +2,53 @@
 
 import { createAutoComplete } from '../module_5/index.js';
 
+
+
 let arr = ['a', 'arr', 'autocomplete', 'branch'];
 
 window.addEventListener("load", function(event) {
-
 	
+	function note(prompts){	
+		while (ul.firstChild) {
+    		ul.removeChild(ul.firstChild);
+		}
+		for (var i = 0; i < prompts.length; i++) {
+			let li = document.createElement("li");
+			li.innerHTML = prompts[i];
+			ul.appendChild(li);
+			
+		}
+	}
 
 	let form = document.createElement('form');
 	form.id = "form";
+	form.autocomplete = "off";
 	let input = document.createElement('input');
 	input.id = "autocomplete";
 	input.placeholder = "search...";
 	input.type = "text";
 	input.name = "text";
-	input.autocomplete =false;
 	form.append(input);
 	document.body.append(form);
+	
+	let ul = document.createElement('ul');
+	document.body.append(ul);
 	
 	let autocomplete = createAutoComplete(arr);
 
 
 	
 	input.addEventListener('keyup', function(event){
-		autocomplete(input.value);
-		console.log(autocomplete(input.value));
+		
+		note(autocomplete(input.value));
 	},false);
 
 	input.addEventListener('focus', function(event){
-		event.preventDefault();
-		console.log("focus");
-	},false);
 
-
-	input.addEventListener('click', function(event){
-		event.preventDefault();
-		console.log("click");
 	},false);
 
 	input.addEventListener('blur', function(event){
-		console.log("blur");
+
 	},false);
 });
 

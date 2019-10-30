dfd6add11dc92ef2b33ba1e14936cfecabd46b7d
Arkhipenka <europeoplebelarus@gmail.com>
2019-10-30 22:52:50 +0300

    implemented messages

--- module_12/js/Dom_nav.js
+++ module_12/js/Dom_nav.js
@@ -1,5 +1,8 @@
 
+let count = 0;
+
 window.addEventListener("load", function(event) {
+
 	//navigation for chat
 	let header = document.createElement('header');
 	header.id = "header";
@@ -19,8 +22,8 @@ window.addEventListener("load", function(event) {
 	let input = document.createElement('input');
 	let button = document.createElement('button');
 	let stat = document.createElement('span');
-	stat.id = "status";
-	section.append(stat);
+	let messages = document.createElement('ul');
+	section.append(messages);
 	form.append(input);
 	form.append(button);
 	section.append(form);
@@ -30,30 +33,61 @@ window.addEventListener("load", function(event) {
 	//footer
 	let footer = document.createElement('footer');
 	document.body.append(footer);
-	
+
 	let status = document.createElement('span');
-	/*const status = document.getElementById('status');
-	const messages = document.getElementById('messages');
-	const form = document.getElementById('form');
-	const input = document.getElementById('input');
 
-	const ws = new WebSocket('ws://localhost:3000');
+	
+	button.addEventListener('click',(event) => {
+		event.preventDefault();
+		const value = input.value;
+		if(count < 100){
+			renderUserMessageToSection(value, messages, true);
+		}
+		
+	})
 
-	function setStatus(value) {
-	    status.innerHTML = value;
-	}
+	this.setInterval(() => {
+		if(count < 100){
+			renderUserMessageToSection('hi', messages, false);
+		}
+		}, Math.random() * 50000 + 1000);
+});
 
-	function printMessage(value) {
-	    const li = document.createElement('li');
+function renderUserMessageToSection(text, messages, messageIsMine) {
+		const message = document.createElement('li');
+		if(messageIsMine){
+			message.className = "MyMessage";
+		}else{
+			message.className ="OtherMessage";
+		}
+		message.textContent = text;
+		messages.append(message);
+		count++;
+		console.log(count);
+}
 
-	    li.innerHTML = value;
-	    messages.appendChild(li);
-	}
+// так будем вызывать
+/*renderMessage({
+	from: String,
+	message: String,
+	id: count, // unique id of message
+	time: Number, // time when message has been received
+});
 
-	ws.onopen = () => setStatus('ONLINE');
 
-	ws.onclose = () => setStatus('DISCONNECTED');
 
-	ws.onmessage = response => printMessage(response.data);*/
-	// body...
-})
+// так описывается функция
+function renderMessage(message) {
+	const li = document.createElement('li');
+	
+	const text = document.createElement('p');
+	text.textContent = message.message;
+
+	const date = document.createElement('span');
+	date.textContent = message.time;
+
+	li.appendChild(text).appendChild(date);
+}
+*/
+
+

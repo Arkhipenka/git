54a408dfa016e86bea244e8ac9506e170f5cc04c
Arkhipenka <europeoplebelarus@gmail.com>
2019-11-10 13:42:04 +0300

    get

--- module_15/index.js
+++ module_15/index.js
@@ -1,2 +1,31 @@
-const fs = require('fs');
+const https = require('https');
 const path = require('path');
+const stream = require('stream');
+
+/*const pathToFile = path.join(__dirname, 'list.pdf');
+const readable = http.createReadStream(pathToFile){
+
+}*/
+
+https.get('https://www.googleapis.com/pagespeedonline/v4/runPagespeed', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+/*;
+
+let counter = 0;
+const writable = http.createWriteSteam(_filename);
+*/
